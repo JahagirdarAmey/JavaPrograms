98907e5b5f02739dc405bc1afa189e46a1140ff7
JahagirdarAmey <ameyjahagirdar6@gmail.com>
2018-11-21 07:36:35 +0530

    Reverse number started

---
+++ src/numbers/ReverseNumber.java
@@ -0,0 +1,26 @@
+package numbers;
+
+/**
+ * Created by USER on 21/11/2018.
+ */
+public class ReverseNumber {
+
+    public static void main(String[] args) {
+
+        int number1 = 12345;
+        int number2 = 1234;
+        int number3 = -1234;
+        int number4 = 0;
+        int number = -0;
+
+        System.out.println(" Input is "+number  +" Output is "+reverseNumber(number));
+        System.out.println(" Input is "+number1  +" Output is "+reverseNumber(number1));
+        System.out.println(" Input is "+number2  +" Output is "+reverseNumber(number2));
+        System.out.println(" Input is "+number3  +" Output is "+reverseNumber(number3));
+        System.out.println(" Input is "+number4  +" Output is "+reverseNumber(number4));
+    }
+
+    private static int reverseNumber(int number) {
+        return 0;
+    }
+}
