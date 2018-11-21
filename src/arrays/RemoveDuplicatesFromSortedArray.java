35b9b98d63e7c3bf7bf8cd7241e0040f78b54185
JahagirdarAmey <ameyjahagirdar6@gmail.com>
2018-11-22 05:18:15 +0530

    Remove duplicates from array started

---
+++ src/arrays/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,25 @@
+package arrays;
+
+import java.util.Arrays;
+
+/**
+ * Created by USER on 22/11/2018.
+ */
+public class RemoveDuplicatesFromSortedArray {
+
+    public static void main(String[] args) {
+        int input[] = {1,2,2,3,4,5,6,7,7,8,9,10};
+
+        System.out.println("Sorted Array : "+ Arrays.toString(input));
+        System.out.println("Sorted Array after removal of duplicates : "+ Arrays.toString(removeDuplicates(input)));
+
+    }
+
+    private static int[] removeDuplicates(int[] input) {
+
+        return input;
+    }
+
+}
+
+// Arrays.toString(input))
