aef2528caff526c2ec42832b5df065a7fbd947e9
JahagirdarAmey <ameyjahagirdar6@gmail.com>
2018-11-22 05:17:46 +0530

    Remove duplicates from array started

---
+++ src/datastructure/search/BinarySearch.java
@@ -0,0 +1,7 @@
+package datastructure.search;
+
+/**
+ * Created by USER on 22/11/2018.
+ */
+public class BinarySearch {
+}
---
+++ src/datastructure/sort/BubbleSort.java
@@ -0,0 +1,26 @@
+package datastructure.sort;
+
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int input[] = {1,54,2,6,8,4,2};
+        System.out.println("Before sorting : "+ Arrays.toString(input));
+        System.out.println("After sorting : "+ Arrays.toString(bubbleSort(input)));
+
+    }
+
+    private static int[] bubbleSort(int[] input) {
+
+        for (int i = 0; i < input.length; i++) {
+
+
+        }
+
+
+        return new int[0];
+    }
+
+}
